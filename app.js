e3d58f6dfc54b2fee0f0f3343e45c3c0f62c693f
Снежана Мацкевич < >
2017-01-21 00:34:55 +0300

    Parsing command line args with yargs

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+/*jshint esversion: 6 */
 console.log("Starting app.js");
 
 const fs = require("fs");
 const _ = require("lodash");
+const yargs = require("yargs");
 
 const notes = require("./notes.js");
+
+
+
+const argv = yargs.argv;
+console.log("Yargs ", argv);
+
+var command = argv._[0];
+
+if (command === "list"){
+  notes.getAll();
+} else if (command === "add") {
+  notes.addNote(argv.title, argv.body);
+} else if (command === "remove"){
+  notes.removeNote(argv.title);
+} else if (command === "read"){
+  notes.readNote(argv.title);
+} else {
+  console.log("Unrecognized command.");
+}
--- notes.js
+++ notes.js
@@ -1,1 +1,26 @@
+/*jshint esversion: 6 */
 console.log("Starting notes.js");
+
+var getAll = () => {
+  console.log("Getting all notes");
+};
+
+var addNote = (title, body) => {
+  console.log("Adding note: ", title, body);
+};
+
+var readNote = (title) => {
+  console.log("Reading note: ", title);
+};
+
+var removeNote = (title) => {
+  console.log("Removing note: ", title);
+};
+
+
+module.exports = {
+  getAll: getAll,
+  addNote: addNote,
+  readNote: readNote,
+  removeNote: removeNote
+};
